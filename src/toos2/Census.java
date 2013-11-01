c19b94e5bc83dd31eab1fea34873f3d82d2f23de
Thomas Boose <thomas@boose.nl>
2013-11-01 14:00:37 +0100

    initial-2

--- src/toos2/Census.java
+++ src/toos2/Census.java
@@ -1,8 +1,6 @@
 package toos2;
 import java.util.ArrayList;
 
-
-
 public interface Census {
 	public boolean voting(ArrayList<Voter> voters) throws Exception ;
 	
--- src/toos2/Voter.java
+++ src/toos2/Voter.java
@@ -1,5 +1,23 @@
 package toos2;
 
 public abstract  class Voter {
-	public abstract boolean vote();
+	public boolean vote;
+	public int voted;
+	
+	public boolean vote() {		
+		return vote;		
+	}
+
+	public Voter(boolean init) {		
+		this.vote = init;
+		this.voted++;
+	}
+	
+	public int getVoted(){
+		return voted;
+	}
+
+	public Voter() {
+		this(true);
+	}
 }
